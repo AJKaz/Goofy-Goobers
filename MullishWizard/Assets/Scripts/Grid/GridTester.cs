518eb8269d7cfa3a17636d8b319018fb1d0e827f
Lucas Corey <89759400+ArgentumRhodon@users.noreply.github.com>
2023-10-06 11:29:58 -0400

    rebuild :')

---
+++ MullishWizard/Assets/Scripts/Grid/GridTester.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GridTester : MonoBehaviour
+{
+    private Grid grid;
+    void Start()
+    {
+        grid = new Grid(9, 9, 1, new Vector3(-4.5f, -4.5f));
+    }
+
+    private void Update()
+    {
+        // Upon clicking the mouse, change the value of the containing grid cell, if any
+        if (PauseControl.isPaused) return;
+
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            grid.SetValue(worldMousePosition, 1);
+        }
+    }
+}
