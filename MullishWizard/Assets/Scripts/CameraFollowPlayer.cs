9b0719c5866d2ea410df6eb64a03b7d4a9077d4d
apowers131 <arthurpow131@gmail.com>
2023-10-05 23:41:35 -0400

    fixed physics issues, added basic pause menu UI

--- MullishWizard/Assets/Scripts/CameraFollowPlayer.cs
+++ MullishWizard/Assets/Scripts/CameraFollowPlayer.cs
@@ -7,12 +7,12 @@ public class CameraFollowPlayer : MonoBehaviour
 {
 
     [SerializeField] private Transform player;
-    [SerializeField] private float maxDistFromPlayer = 10.0f;
+    [SerializeField] private float smoothness = 20.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        setPosition(player.position);
+        SetPosition(player.position);
     }
 
     // Update is called once per frame
@@ -21,16 +21,19 @@ public class CameraFollowPlayer : MonoBehaviour
         // This is so cursed. Eventually, refactor this to reduce camera stuttering
         // and to actually make sense in a 2d context
 
-        if ((player.position - transform.position).sqrMagnitude > maxDistFromPlayer * maxDistFromPlayer)
-        {
-            Vector3 direction = (player.position - transform.position).normalized;
+        //if ((player.position - transform.position).sqrMagnitude > maxDistFromPlayer * maxDistFromPlayer)
+        //{
+        //    Vector3 direction = (player.position - transform.position).normalized;
 
-            setPosition(player.position - direction * maxDistFromPlayer);
-            DebugCanvas.AddDebugText("Cam direction", direction.ToString());
-        }
+        //    SetPosition(player.position - direction * maxDistFromPlayer);
+        //    DebugCanvas.AddDebugText("Cam direction", direction.ToString());
+        //}
+
+        Vector2 currentPosition = Vector2.Lerp(transform.position, player.position, smoothness * Time.deltaTime);
+        SetPosition(currentPosition);
     }
 
-    private void setPosition(Vector3 position)
+    private void SetPosition(Vector3 position)
     {
         transform.position = new Vector3(position.x, position.y, -10);
     }
