d9f36f7a514c21e58b8e256becf0222b18488ea0
AJKaz <ajkazazian@gmail.com>
2023-10-30 20:00:33 -0400

    Enemy pathfinding with waypoints

--- Defend The Divine/Assets/Scripts/Enemies/Enemy.cs
+++ Defend The Divine/Assets/Scripts/Enemies/Enemy.cs
@@ -3,21 +3,44 @@ using UnityEngine;
 public class Enemy : Entity {
 
     [SerializeField]
-    private float damage = 10.0f;
+    protected float damage = 10.0f;
 
     [SerializeField]
-    private float moveSpeed = 2.0f;
+    protected float moveSpeed = 1.0f;
+
+    protected int waypointIndex = 0;
 
     protected Enemy enemyComponent;
     protected SpriteRenderer sprite;
+
+    protected Transform[] path;
 
     private void Awake() {
         enemyComponent = GetComponent<Enemy>();
         sprite = GetComponentInChildren<SpriteRenderer>();
     }
 
+    protected override void Start() {
+        base.Start();
+        path = GameManager.Instance.GetRandomPath();
+        transform.position = path[waypointIndex].transform.position;
+    }
+
     void Update() {
-        
+        Pathfind();
+    }
+
+    protected void Pathfind() {
+        if (waypointIndex < path.Length) {
+            // Move enemy from current waypoint to next one
+            transform.position = Vector2.MoveTowards(transform.position,
+                path[waypointIndex].transform.position, moveSpeed * Time.deltaTime);
+
+            // When enemy reaches next waypoint, increase waypointIndex so they can walk to next one
+            if (transform.position == path[waypointIndex].transform.position) {
+                waypointIndex++;
+            }
+        }
     }
 
     override protected void Die() {
--- Defend The Divine/Assets/Scripts/GameManager.cs
+++ Defend The Divine/Assets/Scripts/GameManager.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -9,11 +11,41 @@ public class GameManager : MonoBehaviour
     [HideInInspector] 
     public List<Enemy> enemies;
 
+    [SerializeField]
+    private Transform[] path1;
+
+    [SerializeField]
+    private Transform[] path2;
+
+    [SerializeField]
+    private Transform[] path3;
+
+    [SerializeField]
+    private int startingMoney = 10;
+
+    private int money;
+
     private void Awake() {
         if (Instance == null) {
             Instance = this;
         }
+        money = startingMoney;
     }
+
+    public Transform[] GetRandomPath() {
+        int randomPath = Random.Range(0, 2);
+        switch (randomPath) {
+            case 0: return path1;
+            case 1: return path2;
+            case 2: return path3;
+            default: return path1;
+        }
+    }
+
+    public void AddMoney(int amount) {
+        money += amount;
+    }
+    public int GetMoney() { return money; }
 
     public bool RemoveEnemy(Enemy enemy) {
         return enemies.Remove(enemy);
