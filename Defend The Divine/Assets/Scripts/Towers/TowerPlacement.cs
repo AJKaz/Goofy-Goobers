b13dd436dce0a6cf639751397b805fe6071b0872
AJKaz <ajkazazian@gmail.com>
2023-11-30 19:46:38 -0500

    Ghost Tower Improvements

--- Defend The Divine/Assets/Scripts/Towers/TowerPlacement.cs
+++ Defend The Divine/Assets/Scripts/Towers/TowerPlacement.cs
@@ -8,15 +8,24 @@ public class TowerPlacement : MonoBehaviour
 {
     [SerializeField]
     private Tower towerPrefab;
-    [SerializeField]
-    private TowerGhost towerGhost;
 
+    [Header("Ghost Towers")]
+    [SerializeField] private TowerGhost tower1Ghost;
+    [SerializeField] private TowerGhost tower2Ghost;
+    [SerializeField] private TowerGhost tower3Ghost;
+    private TowerGhost currentTowerGhost;
     private bool canPlaceTower = true;
 
     public enum TowerType {tower1, tower2, tower3 };
+    [Header("Towers")]
     [SerializeField] Tower towerType1Prefab;
     [SerializeField] Tower towerType2Prefab;
     [SerializeField] Tower towerType3Prefab;
+
+    private void Awake() {
+        /* REMOVE THIS ONCE ONE CLICK PER TOWER PLACEMENT IS IMPLEMENTED */
+        currentTowerGhost = tower1Ghost;
+    }
 
     void Update()
     {
@@ -28,26 +37,25 @@ public class TowerPlacement : MonoBehaviour
          */
         canPlaceTower =
             GameManager.Instance.Money >= towerPrefab.Cost
-            && !towerGhost.CollidingWithPath
+            && !currentTowerGhost.CollidingWithPath
             && !GameManager.Instance.GetComponent<MouseUICheck>().IsPointerOverUIElement();
 
         Vector2 currentMousePosition = GameManager.Instance.inputManager.MouseWorldPosition;
-        towerGhost.transform.position = new Vector3(currentMousePosition.x, currentMousePosition.y, 0);
+        currentTowerGhost.transform.position = new Vector3(currentMousePosition.x, currentMousePosition.y, 0);
 
         // Show when a tower can be placed
-        SpriteRenderer towerGhostSR = towerGhost.GetComponent<SpriteRenderer>();
-        if (canPlaceTower)
+        SpriteRenderer towerGhostSR = currentTowerGhost.GetComponent<SpriteRenderer>();
+        if (!canPlaceTower)
         {
-            towerGhostSR.color = new Color(0f, 1f, 0f);
+            towerGhostSR.color = new Color(0.8f, 0f, 0f, 0.8f);
         }
-        else
-        {
-            towerGhostSR.color = new Color(1f, 0f, 0f);
+        else {
+            towerGhostSR.color = new Color(1f, 1f, 1f, 0.8f);
         }
 
         if (GameManager.Instance.inputManager.MouseLeftDownThisFrame && canPlaceTower)
         {
-            GameObject.Instantiate(towerPrefab, towerGhost.transform.position, Quaternion.identity);
+            GameObject.Instantiate(towerPrefab, currentTowerGhost.transform.position, Quaternion.identity);
             GameManager.Instance.AddMoney(-towerPrefab.Cost);
         }
     }
@@ -55,13 +63,19 @@ public class TowerPlacement : MonoBehaviour
     public void SetCurrentTowerType(TowerType towerType) {
         switch (towerType) {
             case TowerType.tower1:
+                currentTowerGhost.transform.position = new Vector2(-9f, 255f);
                 towerPrefab = towerType1Prefab;
+                currentTowerGhost = tower1Ghost;
                 break;
             case TowerType.tower2:
+                currentTowerGhost.transform.position = new Vector2(0f, 255f);
                 towerPrefab = towerType2Prefab;
+                currentTowerGhost = tower2Ghost;
                 break;
             case TowerType.tower3:
+                currentTowerGhost.transform.position = new Vector2(9f, 255f);
                 towerPrefab = towerType3Prefab;
+                currentTowerGhost = tower3Ghost;
                 break;
             default:
                 break;
