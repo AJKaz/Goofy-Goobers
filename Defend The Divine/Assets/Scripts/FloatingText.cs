5f40aa736870986fb608f80cea3546ae8c5ba599
Lucas Corey <89759400+ArgentumRhodon@users.noreply.github.com>
2023-12-06 14:00:07 -0500

    Changed font sizes, varied floating damage pos

--- Defend The Divine/Assets/Scripts/FloatingText.cs
+++ Defend The Divine/Assets/Scripts/FloatingText.cs
@@ -5,15 +5,32 @@ using UnityEngine;
 public class FloatingText : MonoBehaviour
 {
     public float DestroyTime = 2f;
+    public float timeSinceInst = 0f;
+    public Vector3 randomOffsetMagnitude = new Vector3(0.15f, 0, 0);
+    public Vector3 initialPosition;
     // Start is called before the first frame update
     void Start()
     {
         Destroy(gameObject, DestroyTime);
         transform.position += new Vector3(0, 0.5f, 0);
+        transform.position += new Vector3(
+            Random.Range(-randomOffsetMagnitude.x, randomOffsetMagnitude.x),
+            Random.Range(-randomOffsetMagnitude.y, randomOffsetMagnitude.y),
+            Random.Range(-randomOffsetMagnitude.z, randomOffsetMagnitude.z)
+        );
+
+        initialPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        timeSinceInst += Time.deltaTime;
+        transform.position = new Vector3(transform.position.x, initialPosition.y + GetCurveProgress(timeSinceInst), transform.position.z);
+    }
+
+    private float GetCurveProgress(float timePassed)
+    {
+        return timePassed;
     }
 }
